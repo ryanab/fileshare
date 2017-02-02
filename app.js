813b36346ab66d84a8d8adfdd4241a33069a4a8b
Ryan Byrne <ryanbyrne@Ryans-MacBook-Pro.local>
2017-02-01 20:30:43 -0500

    Created Profile Controller, Implemented Client Sessions in App.js, and created Get actions in Account Route Handler

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sessions = require('client-sessions')
+
 var mongoose = require('mongoose')
 require('dotenv').config()
 
@@ -17,7 +19,7 @@ mongoose.connect(dbUrl, function(err){
 })
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var account = require('./routes/account');
 
 var app = express();
 
@@ -31,10 +33,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(sessions({
+  cookieName: 'session',
+  secret: process.env.SESSION_SECRET,
+  duration: 24*60*60*1000, // 1 day
+  activeDuration:30*60*1000
+}))
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/account', account);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/account.js
@@ -0,0 +1,66 @@
+var express = require('express')
+var router = express.Router()
+var jwt = require('jsonwebtoken')
+var ProfileController = require('../controllers/ProfileController')
+
+/* GET users listing. */
+router.get('/:action', function(req, res, next) {
+  var action = req.params.action
+
+  if(action=='currentuser'){
+    if(req.session == null){
+      res.json({
+        confirmation: 'success',
+        user: null
+      })
+      return
+    }
+
+    if(req.session.token==null){
+      res.json({
+        confirmation: 'success',
+        user: null
+      })
+      return
+    }
+
+    jwt.verify(req.session.token, process.env.TOKEN_SECRET, function(err, decoded){
+      if(err){
+        req.session.reset()
+        res.json({
+          confirmation: 'success',
+          user: null
+        })
+        return
+      }
+      ProfileController
+      .getById(decoded.id, false)
+      .then(function(result){
+        res.json({
+          confirmation: 'success',
+          user: result
+        })
+        return
+      })
+      .catch(function(err){
+        res.json({
+          confirmation: 'fail',
+          message: err
+        })
+        return
+      })
+    })
+  }
+
+  if(action=='logout'){
+    req.session.reset()
+    res.json({
+      confirmation: 'success',
+      user: null
+    })
+    return
+  }
+})
+
+
+module.exports = router
