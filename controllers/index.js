e62bb7ae4091da54b8968de5aef568b4bc2a5931
Rob Ungar <robungar@gmail.com>
2017-02-01 20:48:01 -0500

    removed profile controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,6 @@
 var FileController = require ('./FileController')
-var ProfileController = require ('./ProfileController')
+
 
 module.exports = {
-	file: FileController,
-	profile: ProfileController
+	file: FileController
 }
