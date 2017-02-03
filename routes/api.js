4a9af0b8860fbe8a5918fee0782f7014b65c5142
Rob Ungar <robungar@gmail.com>
2017-02-02 22:36:03 -0500

    finished api route

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,8 @@ var controllers = require('../controllers')
 /* GET users listing. */
 router.get('/:resource', function(req, res, next) {
   var resource = req.params.resource
-
-  var controller = controllers[resource]
+	var controller = controllers[resource]
+  
   if(controller == null){
   	res.json({
   		confirmation: 'fail',
@@ -14,19 +14,73 @@ router.get('/:resource', function(req, res, next) {
   	})
   	return
   }
+  
   controller.get(req.query, false)
   .then(function(results){
   	res.json({
   		confirmation: 'success',
   		results: results
   	})
+  })
   	.catch(function(err){
   		res.json({
   			confirmation: 'fail',
   			message: err
   		})
   	})
-  })
+ })
+
+router.get('/:resource/:id', function(req, res, next){
+	var resource = req.params.resource
+	var controller = controllers[resource]
+	
+	if(controller == null){
+		res.json({
+			confirmation: 'fail',
+			message: 'Invalid Resource'
+		})
+		return
+	}
+
+	controller.getById(req.params.id, false)
+	.then(function(result){
+		res.json({
+			confirmation: 'success',
+			result: result
+		})
+	})
+	.catch(function(err){
+		res.json({
+			confirmation: 'fail',
+			message: 'Not found'
+		})
+	})
 })
 
+router.post('/:resource', function(req, res, next){
+	var resource = req.params.resource
+	var controller = controllers[resource]
+
+	if(controller == null){
+		res.json({
+			confirmation: 'fail',
+			message: 'Invalid Resource'
+		})
+		return
+	}
+
+	controller.post(req.body, false)
+	.then(function(result){
+		res.json({
+			confirmation: 'success',
+			result: result
+		})
+	})
+	.catch(function(err){
+		res.json({
+			confirmation: 'fail',
+			message: err
+		})
+	})
+})
 module.exports = router;
