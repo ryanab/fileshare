a1e40be40ea0ff633e629e59a592d12b3a3f2c14
Forrest Filler <forrestfiller@users.noreply.github.com>
2017-02-04 14:25:01 -0500

    rebase and conflict resolution from merge

---
+++ src/components/layout/ProfileStatic.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Profile } from '../containers'
+
+class ProfileStatic extends Component {
+	render(){
+		return(
+			<div>
+				ProfileStatic Layout
+				<Profile />
+			</div>
+		)
+	}
+}
+
+export default ProfileStatic
