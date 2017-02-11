203c421f15db4768727af4e6f25375241450437f
jdrichardstech <jdrichardstech@gmail.com>
2017-02-11 12:34:02 -0500

    branch back in worker order

--- src/components/view/CreateFile.js
+++ src/components/view/CreateFile.js
@@ -1,23 +1,10 @@
+
 import React, { Component } from 'react'
 import DropZone from 'react-dropzone'
 import sha1 from 'sha1'
 import { APIManager } from '../../utils'
 
 class CreateFile extends Component{
-<<<<<<< HEAD
-	constructor(){
-		super()
-		this.state={
-			post:{
-				file:[],
-				fileType:''
-			}
-		}
-	}
-
-  fileSelected(files){
-		console.log("FILES: "+ JSON.stringify(files.length))
-=======
 
 	updateFileInfo(key, event){
 		const value = event.target.value
@@ -29,7 +16,6 @@ class CreateFile extends Component{
 	}
 
   fileSelected(files){
->>>>>>> c908935ee4e0fe572ef3ce7d211dbc32f9012ebe
 			if(files.length == 0){
 				alert('File is too large')
 				return
@@ -50,18 +36,9 @@ class CreateFile extends Component{
 
 		APIManager.uploadFile(url, selectedFile, params)
 		.then((result) => {
-<<<<<<< HEAD
-			let updated = Object.assign({}, this.state.post)
-			updated['file'].unshift(result['secure_url'])
-			updated['fileType']= result.format
-			this.setState({
-				post: updated
-			})
-=======
 			this.props.updateFileInfo('fileUrl', result['secure_url'])
 			this.props.updateFileInfo('fileExtension', result['format'])
-			return null 
->>>>>>> c908935ee4e0fe572ef3ce7d211dbc32f9012ebe
+			return null
 		})
 		.catch((err) => {
 			alert(err.message)
@@ -69,16 +46,6 @@ class CreateFile extends Component{
 	}
 
   render(){
-<<<<<<< HEAD
-		const post = this.state.post
-		const imageExtensionTypes=['img','jpg','gif','png','jpeg','tif','bmp']
-		const renderUploadSuccess = (imageExtensionTypes.indexOf(post.fileType)==-1) ?
-			<h2>Upload Successful</h2> : <img src={post.file[0]} />
-		const fileSuccessMessage = (post.file.length==0) ? null : <div>{renderUploadSuccess}</div>
-
-    return(
-      <div>
-=======
 		// const file = this.state.file
 		// const imageExtensionTypes=['img','jpg','gif','png','jpeg','tif','bmp']
 		// const renderUploadSuccess = (imageExtensionTypes.indexOf(file.fileType)==-1) ?
@@ -89,16 +56,11 @@ class CreateFile extends Component{
       <div>
       	<input onChange={this.updateFileInfo.bind(this, 'fileTitle')} placeholder="File Title" type="text" /><br />
       	<input onChange={this.updateFileInfo.bind(this, 'fileDescription')} placeholder="File Description" type="text" /><br />
->>>>>>> c908935ee4e0fe572ef3ce7d211dbc32f9012ebe
         <DropZone style={{border:'none'}} onDrop={this.fileSelected.bind(this)} maxSize={10000000}>
         	<button>Upload File</button>
         </DropZone>
 				<br />
-<<<<<<< HEAD
-				{fileSuccessMessage}
-=======
 				<button type="submit" onClick={this.createFile.bind(this)}>Create File</button>
->>>>>>> c908935ee4e0fe572ef3ce7d211dbc32f9012ebe
       </div>
     )
   }
