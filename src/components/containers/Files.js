a8e50bb047cec73a6f03718364135014326821a3
jdrichardstech <jdrichardstech@gmail.com>
2017-02-14 18:35:04 -0500

    indents and code cleaning

--- src/components/containers/Files.js
+++ src/components/containers/Files.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux'
 
 class Files extends Component{
 
-
   constructor(){
     super()
     this.state = {
@@ -17,10 +16,7 @@ class Files extends Component{
 
   componentDidMount(){
     this.props.fetchFiles({})
-
-
   }
-
 
   createFile(){
     event.preventDefault()
@@ -34,68 +30,59 @@ class Files extends Component{
 
 		const file = this.state.file
 		const fileUrl = file['fileUrl']
+		const image = ['jpg','png','gif','bmp','jpeg']
+		const video = ['webm', 'mp4','ogv','mov']
+		const audio = ['mp3', 'm4a','wav','ogg','aif']
 		let extension = ''
 		if(fileUrl != undefined){
 			extension = fileUrl.substring(fileUrl.length-3)
 		}
 
-    // console.log("EXTENSION: " + extension)
-
-		const image = ['jpg','png','gif','bmp','jpeg']
-		const video = ['webm', 'mp4','ogv','mov']
-		const audio = ['mp3', 'm4a','wav','ogg','aif']
-
-
-
- let updated = Object.assign({}, this.state.file)
-		if(image.indexOf(extension) > -1){
-			updated['fileCategory'] = 'image'
-		}else if(video.indexOf(extension) > -1){
-			updated['fileCategory'] = 'video'
-		}else if(audio.indexOf(extension) > -1){
-			updated['fileCategory'] = 'audio'
-		}else if(extension =='pdf'){
-			updated['fileCategory'] = 'pdf'
-		}else{
-			updated['fileCategory'] = 'misc'
-		}
-		// console.log("FILE CATEGORY: " + JSON.stringify(updated['fileCategory']))
-    // updated[key] = value
-    this.setState({
-      file: updated
-    })
+	 	let updated = Object.assign({}, this.state.file)
+			if(image.indexOf(extension) > -1){
+				updated['fileCategory'] = 'image'
+			}else if(video.indexOf(extension) > -1){
+				updated['fileCategory'] = 'video'
+			}else if(audio.indexOf(extension) > -1){
+				updated['fileCategory'] = 'audio'
+			}else if(extension =='pdf'){
+				updated['fileCategory'] = 'pdf'
+			}else{
+				updated['fileCategory'] = 'misc'
+			}
+		  // updated[key] = value ????? Ryan what is this for?
+		this.setState({
+		  file: updated
+		})
   }
 
   render(){
-		const files = this.props.files.completeFileList
-		const iconType = null
-		console.log("FILES: " + JSON.stringify(this.props.files))
-
 		const fileTypeIcons = ["fa fa-file-picture-o fa-3x","fa-file-movie-o fa-3x","fa fa-file-pdf-o fa-3x","fa fa-file-audio-o fa-3x","fa fa-question-circle-o fa-3x"]
 		const fileCategories = ['image','video','pdf','audio','misc']
 
     return(
       <div>
 				<ol>
-					{ (this.props.files == null ) ? <h2>nothing found</h2> :
+					{
+						(this.props.files == null ) ? 'No Files Rendered'
+						:
 						this.props.files.map((file,i)=>{
 							return(
 								<div key={i}>
-									<li><i className={fileTypeIcons[fileCategories.indexOf(file.fileCategory)]} style={{paddingRight:10}}></i>
-									 {file.fileTitle} created by {(file.profile['firstName'] != '') ?  <span>{file.profile['firstName']}</span> : 'anonymous'}
-								 		<br /><br />
-								 		{(file.fileCategory=='image') ? <span><img src={file.fileUrl} /></span> : null}
-								 		<br /><br />
-									</li>
-								</div>
-							)
-						})
-					}
-			</ol>
+										<li><i className={fileTypeIcons[fileCategories.indexOf(file.fileCategory)]} style={{paddingRight:10}}></i>
+										 {file.fileTitle} created by {(file.profile['firstName'] != '') ?  <span>{file.profile['firstName']}</span> : 'anonymous'}
+									 		<br /><br />
+									 		{(file.fileCategory=='image') ? <span><img src={file.fileUrl} /></span> : null}
+									 		<br /><br />
+										</li>
+									</div>
+								)
+							})
+						}
+				</ol>
 
         <File  files={this.props.file}/>< br />
         <CreateFile createFile={this.createFile.bind(this)} updateFileInfo={this.updateFileInfo.bind(this)}/>
-
 			</div>
     )
   }
@@ -110,10 +97,8 @@ const stateToProps = (state) => {
 
 const dispatchToProps = (dispatch) => {
   return {
-		fetchCurrentUser:()=> dispatch(actions.fetchCurrentUser()),
     fetchFiles: (params) => dispatch(actions.fetchFiles(params)),
     createFile: (params) => dispatch(actions.createFile(params)),
-
   }
 }
 
