cf663915cfca14367effeb202698927d9dc7a688
jdrichardstech <jdrichardstech@gmail.com>
2017-02-14 17:46:34 -0500

    accounted for userName being null in render and changed icon size to 3x

--- src/components/containers/Files.js
+++ src/components/containers/Files.js
@@ -10,22 +10,15 @@ class Files extends Component{
     super()
     this.state = {
       file:{
-         //default for now until we write function to determine fileType
+
       }
     }
   }
 
   componentDidMount(){
-
-
     this.props.fetchFiles({})
-	//  console.log("FILESFETCHED:" + JSON.stringify(this.props.files))
-  }
-	componentDidUpdate(){
-		//  console.log("USER: " + JSON.stringify(this.props.user))
-		//  console.log("FILESFETCHED: " + JSON.stringify(this.props.files))
-	}
 
+  }
 
   createFile(){
     event.preventDefault()
@@ -44,7 +37,7 @@ class Files extends Component{
 			extension = fileUrl.substring(fileUrl.length-3)
 		}
 
-    console.log("EXTENSION: " + extension)
+    // console.log("EXTENSION: " + extension)
 
 		const image = ['jpg','png','gif','bmp','jpeg']
 		const video = ['webm', 'mp4','ogv','mov']
@@ -54,46 +47,40 @@ class Files extends Component{
 
  let updated = Object.assign({}, this.state.file)
 		if(image.indexOf(extension) > -1){
-			console.log("THIS IS AN IMAGE")
 			updated['fileCategory'] = 'image'
 		}else if(video.indexOf(extension) > -1){
 			updated['fileCategory'] = 'video'
-			console.log("THIS IS AN VIDEO")
 		}else if(audio.indexOf(extension) > -1){
 			updated['fileCategory'] = 'audio'
-			console.log("THIS IS AUDIO")
 		}else if(extension =='pdf'){
 			updated['fileCategory'] = 'pdf'
-			console.log("THIS IS AN PDF")
 		}else{
 			updated['fileCategory'] = 'misc'
-			console.log("THIS IS AN MISC")
 		}
-
-		console.log("FILE CATEGORY: " + JSON.stringify(updated['fileCategory']))
-    updated[key] = value
+		// console.log("FILE CATEGORY: " + JSON.stringify(updated['fileCategory']))
+    // updated[key] = value
     this.setState({
       file: updated
     })
-
   }
 
   render(){
 		const files = this.props.files.completeFileList
-		 console.log("FILES: " + JSON.stringify(this.props.files.types))
-
-	 	const fileTypeIcon = ["fa fa-file-pdf-o fa-2x","fa-file-movie-o fa-2x","fa fa-file-audio-o fa-2x","fa fa-question-circle fa-2x"]
+		const iconType = null
+		console.log("FILES: " + JSON.stringify(this.props.files))
+		const userName = (this.props.user == null) ? 'anonymous':<span> {this.props.user.firstName}</span>
+	 	const fileTypeIcon = ["fa fa-file-picture-o fa-3x" ,"fa fa-file-pdf-o fa-3x","fa-file-movie-o fa-3x","fa fa-file-audio-o fa-3x","fa fa-question-circle fa-3x"]
 		let fileType = this.state.file.fileCategory
+		console.log("FILETYPE IN RENDER: " + (JSON.stringify(fileType)))
     return(
       <div>
-
 				<ol>
 					{ (this.props.files == null ) ? <h2>nothing found</h2> :
 						this.props.files.map((file,i)=>{
 							return(
 								<div key={i}>
 									<li><i className={fileTypeIcon[0]} style={{paddingRight:10}}></i>
-									 {file.fileTitle} created by {this.props.user.firstName}<br /><br />
+									 {file.fileTitle} created by {userName}<br /><br />
 								 <img src={file.fileUrl} /><br /><br />
 									</li>
 								</div>
